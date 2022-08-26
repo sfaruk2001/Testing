ff1427d5d1402b6a9f65eec6e8a5925ecfda520e
shihabfaruk <sfaruk3131p@gmail.com>
2022-08-26 19:31:52 -0400

    goodbye update

--- src/g/Hello.java
+++ src/g/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("g.Hello");
         System.out.println("Goodbye");
+        System.out.println("Everyone");
     }
 }
